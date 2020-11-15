cdecf7b999e7361cb66c6dfe5f9d8a1083ec0969
kksb0831 <kksb0832@naver.com>
2020-11-15 21:04:45 +0900

    11/15 오늘의 마무리

--- JAVA_Practice/src/Baseball/Gamego.java
+++ JAVA_Practice/src/Baseball/Gamego.java
@@ -1,21 +1,48 @@
 package Baseball;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 //숫자 입력, 게임시작
 class Gamego {
+    public static void main(String[] args) {
+        Gamego.insertnum();
+    }
+
+
     static int[] gamego(int[] answer) {
+        System.out.println("게임을 시작합니다!!");
+        int[] correct = Gamego.insertnum();
+        Calculate.calculate(correct,answer);
+        return correct;
+    }
+
+    static int[] insertnum() {
         int[] correct = new int[3];
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("게임을 시작합니다!!");
-        System.out.print("숫자를 입력해주세요 : ");
 
-        for (int i = 0; i < correct.length; i++){
-            correct[i] = scanner.nextInt();
+        System.out.print("서로 다른 숫자 세자리를 입력하세요 : ");
+        String num = scanner.nextLine();
+        System.out.println(Integer.parseInt(num));
+
+        num = Gamego.numcheck(num);
+        if (num != null) {
+            correct[2] = Integer.parseInt(num) % 10;
+            correct[1] = ((Integer.parseInt(num) % 100) - correct[2]) / 10;
+            correct[0] = (Integer.parseInt(num) - (correct[1] * 10) + correct[2]) / 100;
+
+            System.out.println(Arrays.toString(correct));
         }
-
-        Calculate.calculate(correct,answer);
         return correct;
+    }
+
+    static String numcheck(String num) {
+        if (num.length() != 3) {
+            System.out.println("숫자는 세자리만 가능합니다");
+            num = null;
+            Gamego.insertnum();
+        }
+        return num;
     }
 }
